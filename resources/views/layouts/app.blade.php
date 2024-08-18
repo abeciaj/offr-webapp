ab4706baa7bb4b4d622fcc92b1884b1225e1e893
Jayllan Abecia <abeciaj23@gmail.com>
2024-08-18 23:12:30 +1000

    Update logo and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" href="{{ asset('images/logo2.png') }}" type="image/png">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -48,7 +50,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    {{ config('app.name', 'Offr App') }}
+                    <!-- {{ config('app.name', 'Offr App') }} -->
+                    <img src="{{ asset('images/logo2.png') }}" alt="logo" class="navbar-logo" style="width: 100px; height: auto;">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
